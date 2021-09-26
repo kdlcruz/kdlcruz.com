8d50ffcceec7281d3efe83eca430e51e3d44c7fe
kdlcruz <kj_delacruz28@yahoo.com>
2021-09-26 18:31:56 +0800

    feat: updated skills

--- src/app/data/client-skills.js
+++ src/app/data/client-skills.js
@@ -1,26 +1,30 @@
 export default [
-	{
-		txt: 'React',
-		percent: '95'
-	},
+  {
+    txt: 'ReactJs',
+    percent: '95'
+  },
+  {
+    txt: 'React Native',
+    percent: '95'
+  },
   {
     txt: 'Ionic Framework',
     percent: '90'
   },
   {
-		txt: 'AngularJS',
-		percent: '85'
-	},
-	{
-		txt: 'VueJs',
-		percent: '85'
-	},
+    txt: 'AngularJS',
+    percent: '85'
+  },
   {
-		txt: 'JQuery',
-		percent: '85'
-	},
+    txt: 'VueJs',
+    percent: '85'
+  },
   {
-		txt: 'Native Javascript',
-		percent: '85'
-	}
+    txt: 'JQuery',
+    percent: '85'
+  },
+  {
+    txt: 'Native Javascript',
+    percent: '85'
+  }
 ]
